10046e64eb36721e2a2c5f6bdd34809bfa801aff
20019616 <20019616@rp.edu.sg>
2021-06-03 18:20:41 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demodynamiclistviewcolourlist/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.demodynamiclistviewcolourlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etElement;
+    EditText etIndexElement;
+    Button btnAdd;
+    ListView lvColour;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etElement = findViewById(R.id.editTextColour);
+        etIndexElement = findViewById(R.id.editTextIndex);
+        btnAdd = findViewById(R.id.buttonAddItem);
+        lvColour = findViewById(R.id.listViewColour);
+
+        ArrayList<String> alColours = new ArrayList<>();
+
+        alColours.add("Red");
+        alColours.add("Orange");
+
+        ArrayAdapter<String> aaColour = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alColours);
+
+        lvColour.setAdapter(aaColour);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String colour = etElement.getText().toString();
+
+                // alColours.add(color);
+                int pos = Integer.parseInt(etIndexElement.getText().toString());
+
+                alColours.add(pos, colour);
+                aaColour.notifyDataSetChanged();
+            }
+        });
+
+        lvColour.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String colour = alColours.get(position);
+
+                Toast.makeText(MainActivity.this, colour + " was selected", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
+
+
+
+
+
+
+
